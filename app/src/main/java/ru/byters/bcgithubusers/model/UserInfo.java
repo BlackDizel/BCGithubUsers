253bc513512b38f7f4037e96c18269c18db499c9
BlackDizel <blackdizel@gmail.com>
2016-01-11 21:36:22 +0300

    add pojo data class

---
+++ app/src/main/java/ru/byters/bcgithubusers/model/UserInfo.java
@@ -0,0 +1,36 @@
+package ru.byters.bcgithubusers.model;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+
+    private int id;
+    private String avatar_url;
+    private String login;
+    private int followers;
+    private int following;
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
