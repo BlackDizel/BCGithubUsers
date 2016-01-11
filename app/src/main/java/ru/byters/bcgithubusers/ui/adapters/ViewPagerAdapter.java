2e2e6ba4cd384c610ebecfd63639408d4be161be
BlackDizel <blackdizel@gmail.com>
2016-01-11 21:37:19 +0300

    move constants

--- app/src/main/java/ru/byters/bcgithubusers/ui/adapters/ViewPagerAdapter.java
+++ app/src/main/java/ru/byters/bcgithubusers/ui/adapters/ViewPagerAdapter.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import java.util.Locale;
 
 import ru.byters.bcgithubusers.R;
+import ru.byters.bcgithubusers.model.ModelUserInfo;
 import ru.byters.bcgithubusers.ui.fragments.FragmentList;
 
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
@@ -23,11 +24,11 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                return FragmentList.newInstance(FragmentList.FILTER_AH);
+                return FragmentList.newInstance(ModelUserInfo.FILTER_AH);
             case 1:
-                return FragmentList.newInstance(FragmentList.FILTER_IP);
+                return FragmentList.newInstance(ModelUserInfo.FILTER_IP);
             case 2:
-                return FragmentList.newInstance(FragmentList.FILTER_QZ);
+                return FragmentList.newInstance(ModelUserInfo.FILTER_QZ);
             default:
                 break;
         }
