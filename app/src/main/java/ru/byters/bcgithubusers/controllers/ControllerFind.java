8c1c5683d1c95c5721b55788a91f62bc05f0e812
BlackDizel <blackdizel@gmail.com>
2016-01-12 01:28:43 +0300

    add search feature

---
+++ app/src/main/java/ru/byters/bcgithubusers/controllers/ControllerFind.java
@@ -0,0 +1,62 @@
+package ru.byters.bcgithubusers.controllers;
+
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ru.byters.bcgithubusers.api.GithubService;
+import ru.byters.bcgithubusers.controllers.utils.OnDownloaded;
+import ru.byters.bcgithubusers.model.UserInfo;
+import ru.byters.bcgithubusers.ui.adapters.UsersListAdapter;
+
+public class ControllerFind implements OnDownloaded {
+
+    UsersListAdapter adapter;
+    private String query;
+
+    public ControllerFind(UsersListAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    public void search(String query) {
+        this.query = query;
+        adapter.resetData();
+        new TaskFind(query, this).execute();
+    }
+
+    @Override
+    public void onDownloaded(String query, ArrayList<UserInfo> data) {
+        if (query.equals(this.query) && data != null)
+            adapter.addData(data);
+    }
+
+    private class TaskFind extends AsyncTask<Void, Void, ArrayList<UserInfo>> {
+
+        String query;
+        OnDownloaded listener;
+
+        public TaskFind(String query, OnDownloaded listener) {
+            this.query = query;
+            this.listener = listener;
+        }
+
+        @Override
+        protected ArrayList<UserInfo> doInBackground(Void... params) {
+            //todo implement v23 check permission
+            try {
+                return GithubService.getApi().searchUsers(query).execute().body().getItems();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<UserInfo> userInfos) {
+            super.onPostExecute(userInfos);
+            listener.onDownloaded(query, userInfos);
+        }
+    }
+}
--- app/src/main/java/ru/byters/bcgithubusers/ui/activities/ActivityMain.java
+++ app/src/main/java/ru/byters/bcgithubusers/ui/activities/ActivityMain.java
@@ -5,11 +5,18 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.internal.view.SupportMenuItem;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import ru.byters.bcgithubusers.R;
+import ru.byters.bcgithubusers.controllers.ControllerFind;
+import ru.byters.bcgithubusers.controllers.ControllerUserInfo;
+import ru.byters.bcgithubusers.ui.adapters.UsersListAdapter;
 import ru.byters.bcgithubusers.ui.adapters.ViewPagerAdapter;
 import ru.byters.bcgithubusers.ui.fragments.FragmentList;
 
@@ -18,18 +25,30 @@ public class ActivityMain extends AppCompatActivity
 
     ViewPagerAdapter adapter;
     ViewPager viewPager;
+    TabLayout tabLayout;
+    RecyclerView rvFound;
+    private boolean isSearchMode;
+    private ControllerFind controllerFind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        isSearchMode = false;
+
+        UsersListAdapter listAdapter = new UsersListAdapter(0, new ControllerUserInfo(this));
+        controllerFind = new ControllerFind(listAdapter);
+
+        rvFound = (RecyclerView) findViewById(R.id.rvFound);
+        rvFound.setAdapter(listAdapter);
+
         viewPager = (ViewPager) findViewById(R.id.pager);
         adapter = new ViewPagerAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         viewPager.setOnPageChangeListener(this);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(viewPager);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -53,14 +72,33 @@ public class ActivityMain extends AppCompatActivity
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            isSearchMode = true;
+            setState();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onQueryTextSubmit(String query) {
-        //todo implement
         return false;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        //todo implement
+        if (TextUtils.isEmpty(newText)) {
+            if (isSearchMode) isSearchMode = false;
+            setState();
+        } else if (!isSearchMode) {
+            isSearchMode = true;
+            setState();
+        }
+
+        if (newText.length() >= 2) {
+            controllerFind.search(newText);
+        }
+
         return true;
     }
 
@@ -77,5 +115,18 @@ public class ActivityMain extends AppCompatActivity
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    void setState() {
+        ((UsersListAdapter) rvFound.getAdapter()).resetData();
+        if (isSearchMode) {
+            viewPager.setVisibility(View.INVISIBLE);
+            tabLayout.setVisibility(View.GONE);
+            rvFound.setVisibility(View.VISIBLE);
+        } else {
+            rvFound.setVisibility(View.INVISIBLE);
+            viewPager.setVisibility(View.VISIBLE);
+            tabLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
