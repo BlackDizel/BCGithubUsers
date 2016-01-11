4e7f2f92b05811a6b1d1447400e87a6539182284
BlackDizel <blackdizel@gmail.com>
2016-01-12 01:51:57 +0300

    add page variable, add task

--- app/src/main/java/ru/byters/bcgithubusers/controllers/ControllerFind.java
+++ app/src/main/java/ru/byters/bcgithubusers/controllers/ControllerFind.java
@@ -15,6 +15,7 @@ public class ControllerFind implements OnDownloaded {
 
     UsersListAdapter adapter;
     private String query;
+    private int page;
 
     public ControllerFind(UsersListAdapter adapter) {
         this.adapter = adapter;
@@ -23,17 +24,20 @@ public class ControllerFind implements OnDownloaded {
     public void search(String query) {
         this.query = query;
         adapter.resetData();
+        page = 1;
         new TaskFind(query, this).execute();
     }
 
     @Override
     public void onDownloaded(String query, ArrayList<UserInfo> data) {
-        if (query.equals(this.query) && data != null)
+        if (query.equals(this.query) && data != null) {
+            ++page;
             adapter.addData(data);
+        }
     }
 
     public void loadMore() {
-        //todo implement
+        new TaskFindMore(query, page, this).execute();
     }
 
     private class TaskFind extends AsyncTask<Void, Void, ArrayList<UserInfo>> {
@@ -51,6 +55,36 @@ public class ControllerFind implements OnDownloaded {
             //todo implement v23 check permission
             try {
                 return GithubService.getApi().searchUsers(query).execute().body().getItems();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<UserInfo> userInfos) {
+            super.onPostExecute(userInfos);
+            listener.onDownloaded(query, userInfos);
+        }
+    }
+
+    private class TaskFindMore extends AsyncTask<Void, Void, ArrayList<UserInfo>> {
+
+        String query;
+        OnDownloaded listener;
+        int page;
+
+        public TaskFindMore(String query, int page, OnDownloaded listener) {
+            this.query = query;
+            this.listener = listener;
+            this.page = page;
+        }
+
+        @Override
+        protected ArrayList<UserInfo> doInBackground(Void... params) {
+            //todo implement v23 check permission
+            try {
+                return GithubService.getApi().searchUsers(query, page).execute().body().getItems();
             } catch (IOException e) {
                 e.printStackTrace();
             }
